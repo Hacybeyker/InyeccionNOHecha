4dbdc4bf00746e6da820c282d15deeddad21a2fc
Carlos Alfredo Osorio Perez <hacybeyker@hotmail.com>
2015-09-28 09:16:45 -0500

    Proyecto inicial sin la implementacion de inyeccion de dependencia

---
+++ src/sininyeccion/Messi.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sininyeccion;
+
+/**
+ *
+ * @author
+ * <AdvanceSoft - Osorio Perez Carlos Alfredo - advancesoft.trujillo@gmail.com>
+ */
+public class Messi {
+    
+    Barcelona barcelona = new Barcelona();
+    
+    public void mostrarEquipo(){
+        barcelona.mostrar();
+    }
+}
